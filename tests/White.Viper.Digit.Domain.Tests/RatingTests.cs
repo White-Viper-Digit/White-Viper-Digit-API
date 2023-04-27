b35bbce12810b67866e0750471cb382b7af9ba77
BOZHI WANG <wbz13204977@gmail.com>
2023-04-27 16:41:54 -0400

    Item Tests

--- tests/White.Viper.Digit.Domain.Tests/RatingTests.cs
+++ tests/White.Viper.Digit.Domain.Tests/RatingTests.cs
@@ -5,6 +5,31 @@ namespace White.Viper.Digit.Domain.Tests;
 public class RatingTests
 {
     [TestMethod]
+    public void Can_Create_New_Item()
+    {
+        var item = new Item("Name", "Description", "Brand", 10.00m);
+
+        Assert.AreEqual("Name", item.Name);
+        Assert.AreEqual("Description", item.Description);
+        Assert.AreEqual("Brand",item.Brand);
+        Assert.AreEqual(10.00m, item.Price);
+    }
+
+    [TestMethod]
+    public void Can_Create_Add_Rating()
+    {
+        //Arrange
+        var item = new Item("Name", "Description", "Brand", 10.00m);
+        var rating = new Rating(5, "Name", "Review");
+
+        //Act
+        item.AddRating(rating);
+
+        //Assert
+        Assert.AreEqual(rating, item.Ratings[0]);
+    }
+
+    [TestMethod]
     public void Can_Create_New_Rating()
     {
         // Arrange
